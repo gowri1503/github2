eacec60608acb5fd339934400678a88d000bc22d
gowri1503 <gowrishankarimani@gmail.com>
2024-08-29 10:42:31 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,5 @@
+try:
+        result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        return e.stderr.strip()
